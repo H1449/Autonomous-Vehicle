b15c169af7f478e683137a8870e7dc796ac356ec
Mohamed Hesham Hassan <mohamedhassan5401@outlook.com>
2024-06-15 23:59:08 +0300

    Cleaned SRVO Driver
    removed unnecessary definitions and added initialization
    but need testing

--- 2-HAL/SERVO/SRVO_PROG.C
+++ 2-HAL/SERVO/SRVO_PROG.C
@@ -9,10 +9,11 @@
 //*************************************************************************************/
 
 //include essential header files (BIT_MATH and STD_TYPES)
-#include <util/delay.h>
+
 #include "BIT_MATH.H"
 #include "STD_TYPES.H"
-#include "AT_REG.H" //this file is written by me to include all essential uncategorized addresses
+#include "_ATMEGA32_.h"  //this file is written by me to include all essential uncategorized addresses
+#include <util/delay.h>
 
 //include header files of the driver
 #include "TMR_CONFIG.H"
@@ -22,34 +23,62 @@
 #include "SRVO_PRIVATE.H"
 #include "SRVO_INTERFACE.H"
 
- f16 pwm = SRVO_HOME_POS_PWM_DS;
- 
-void SRVO_vInit()
+void SRVO_vInit(u8 SRVO_PIN)
 {
-    TMR_TMR0_PWM_vInit(TMR_PRESCALE_64,FALSE);
-   
+    GlobalInterrupt_Enable();
+    switch (SRVO_PIN)
+    {
+        case SRVO_PIN_PB3:
+            TMR0_vInit(TMR_MODE_FAST_PWM, TMR_PRESCALE_1024);
+            TMR0_PWM_vASSIGN_INVERSION(TRUE);
+            TMR0_vENABLE_CTC_INTERRUPT();
+        break;
+
+        case SRVO_PIN_PD7:
+            TMR2_vInit(TMR_MODE_FAST_PWM, TMR_PRESCALE_1024);
+            TMR2_PWM_vASSIGN_INVERSION(TRUE);
+            TMR2_vENABLE_CTC_INTERRUPT();
+        break;
+
+        case SRVO_PIN_PD5:
+            TMR1_vInit(TMR_MODE_FAST_PWM, TMR_PRESCALE_1024);
+            TMR1_PWM_vASSIGN_INVERSION(TRUE);
+            TMR1_vENABLE_CTCA_INTERRUPT();
+        break;
+
+        case SRVO_PIN_PD4:
+            TMR1_vInit(TMR_MODE_FAST_PWM, TMR_PRESCALE_1024);
+            TMR1_PWM_vASSIGN_INVERSION(TRUE);
+            TMR1_vENABLE_CTCB_INTERRUPT();
+        break;
+
+    }
+
+
 }
 
 void SRVO_SWEEP( u8 angle, u8 Direction , u8 Angular_Speed )
 {
+    const u8 SRVO_delay = (u8) (Angular_Speed* SRVO_HOME_POS_PWM_DUTY_CYCLE);
     switch (Direction)
     {
     case SRVO_DIRECTION_ANTICLKWISE:
-    for (pwm= SRVO_HOME_POS_PWM_DS ;  pwm < SRVO_MAX_SERVE ; pwm+= SRVO_ANGLE_STEP_PWM_DS)
+    for (f16 pwm= SRVO_HOME_POS_PWM_DUTY_CYCLE ;  
+    pwm < SRVO_MAX_SERVE ; pwm+= SRVO_ANGLE_STEP_PWM_DUTY_CYCLE)
     {
         TMR_TMR0_PWM_SET_DUTY_CYCLE(pwm);
-        _delay_ms( Angular_Speed *  SRVO_HOME_POS_PWM_DS );
+        _delay_ms( SRVO_delay );
     }
     break;
         case SRVO_DIRECTION_CLKWISE:
-    for (pwm= SRVO_HOME_POS_PWM_DS ;  pwm > SRVO_MIN_SERVE ; pwm-= SRVO_ANGLE_STEP_PWM_DS)
+    for (f16 pwm= SRVO_HOME_POS_PWM_DUTY_CYCLE ; 
+    pwm < SRVO_MAX_SERVE ; pwm+= SRVO_ANGLE_STEP_PWM_DUTY_CYCLE)
     {
-            TMR_TMR0_PWM_SET_DUTY_CYCLE(pwm);
-        _delay_ms( Angular_Speed *  SRVO_HOME_POS_PWM_DS );
+        TMR_TMR0_PWM_SET_DUTY_CYCLE(pwm);
+        _delay_ms( SRVO_delay );
     }
     break;
 
-    default: break;
     }
 }
 
