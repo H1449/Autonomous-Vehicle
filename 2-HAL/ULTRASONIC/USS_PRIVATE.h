0381c2440c5a41877f1462e5202aff4819ad9512
Mohamed Hesham Hassan <mohamedhassan5401@outlook.com>
2024-06-29 21:43:18 +0300

    Fix some errors in USS
    1. fixed trigger function cant trigger the US module by using switch case instead of the trigger variable
    2. fixed echo pin is not being read by echo variable (switch case is also used)
    3. fixed some bugs and so...
    4. need testing

--- 2-HAL/ULTRASONIC/USS_PRIVATE.h
+++ 2-HAL/ULTRASONIC/USS_PRIVATE.h
@@ -12,13 +12,14 @@
 
 #define USS_PRIVATE_H
 
-#define USS_SPEED_OF_SOUND_mm_PER_ms 343
+#define USS_SPEED_OF_SOUND_mm_PER_ms 340
 #define USS_OCR_VALUE 20
-#define USS_TIME_TICK_TIME_s TMR2_PRESCALE/F_CPU
-#define USS_PERIOD_ms (USS_OCR_VALUE*USS_TIME_TICK_TIME_s*1000)
+#define USS_TIME_TICK_TIME_s (f32) (8/F_CPU)
+#define USS_PERIOD_ms (f32) (USS_OCR_VALUE*USS_TIME_TICK_TIME_s*1000)
 #define USS_MEASURE_DISTANCE_mm(TIME_ms)   (u32) ( (USS_SPEED_OF_SOUND_mm_PER_ms/2)*TIME_ms)
-static u8 TRIGGER_PORT;
-static u8 ECHO_PIN;
+#define USS_MIN_PERIOD_ms 0.1
+#define USS_MAX_PERIOD_ms 23324
+
 static u16 counter = 0;
 static f32 time_ms = 0.0;
 
--- 2-HAL/ULTRASONIC/USS_PROG.c
+++ 2-HAL/ULTRASONIC/USS_PROG.c
@@ -22,76 +22,108 @@
 #include "USS_PRIVATE.h"
 #include "USS_INTERFACE.h"
 
+  
 
 static void uSS_vRead_Time(void)
 {
-    counter++;
-    if( GET_BIT(ECHO_PIN, USS_ECHO_PIN) == LOW )
+    u8 Is_Echoed =LOW;
+
+    switch (USS_ECHO_PORT)
+        {
+        case 'A' : Is_Echoed = GET_BIT(GPIO_PINA, USS_ECHO_PIN );break;
+
+        case 'B' : Is_Echoed = GET_BIT(GPIO_PINB, USS_ECHO_PIN );break;
+
+        case 'C' : Is_Echoed = GET_BIT(GPIO_PINC, USS_ECHO_PIN );break;
+
+        case 'D' : Is_Echoed = GET_BIT(GPIO_PIND, USS_ECHO_PIN );break;
+        }
+
+    if( Is_Echoed == LOW && counter!=0 )
     {
-        GlobalInterrupt_Disable();
-        time_ms = (counter*USS_PERIOD_ms) + (TMR_TCNT2*USS_TIME_TICK_TIME_s*1000);
+        TMR2_vDISABLE_CTC_INTERRUPT();
+
         counter = 0;
     }
+    counter++;
 }
 
 void USS_vInit(void)
 {
-
+    
+    GlobalInterrupt_Enable();
+    TMR2_vDISABLE_CTC_INTERRUPT();
     TMR2_vInit(TMR_MODE_CTC, TMR_PRESCALE_8);
-    TMR2_vReset_Prescaler();
+    TMR2_vASSIGN_CTC_VALUE(USS_OCR_VALUE);
     TMR2_vSET_CTC_CALLBACK(&uSS_vRead_Time);
 
     #if USS_TRIG_PORT == 'A'
         SET_BIT(GPIO_DDRA, USS_TRIG_PIN );
-        TRIGGER_PORT=GPIO_PORTA;
+
     #elif USS_TRIG_PORT == 'B'
         SET_BIT(GPIO_DDRB, USS_TRIG_PIN );
-        TRIGGER_PORT=GPIO_PORTB;
+
     #elif USS_TRIG_PORT == 'C'
         SET_BIT(GPIO_DDRC, USS_TRIG_PIN );
-        TRIGGER_PORT=GPIO_PORTC;
+
     #elif USS_TRIG_PORT == 'D'
         SET_BIT(GPIO_DDRD, USS_TRIG_PIN );
-        TRIGGER_PORT=GPIO_PORTD;
+
     #endif
 
     #if USS_ECHO_PORT == 'A'
         CLEAR_BIT(GPIO_DDRA, USS_ECHO_PIN );
-        ECHO_PIN = GPIO_PINA;
+
     #elif USS_ECHO_PORT == 'B'
         CLEAR_BIT(GPIO_DDRB, USS_ECHO_PIN );
-        ECHO_PIN = GPIO_PINB;
+
     #elif USS_ECHO_PORT == 'C'
         CLEAR_BIT(GPIO_DDRC, USS_ECHO_PIN );
-        ECHO_PIN = GPIO_PINC;
+
     #elif USS_ECHO_PORT == 'D'
         CLEAR_BIT(GPIO_DDRD, USS_ECHO_PIN );
-        ECHO_PIN = GPIO_PIND;
+
     #endif
 }
 
 static void USS_vPULL_THE_TRIGGER(void)
 {/*Send 10us pulse to trigger measurement*/
-    CLEAR_BIT(TRIGGER_PORT , USS_TRIG_PIN);
-    _delay_us(2);
-    SET_BIT(TRIGGER_PORT , USS_TRIG_PIN);
-    _delay_us(10);
-    CLEAR_BIT(TRIGGER_PORT , USS_TRIG_PIN);
-    _delay_us(2);
+    switch (USS_TRIG_PORT)
+    {
+    case 'A' :
+        CLEAR_BIT(GPIO_PORTA , USS_TRIG_PIN);_delay_us(20);
+        SET_BIT(GPIO_PORTA, USS_TRIG_PIN );     _delay_us(25);
+        CLEAR_BIT(GPIO_PORTA , USS_TRIG_PIN); _delay_us(20);
+        break;
+    case 'B' :
+        CLEAR_BIT(GPIO_PORTB , USS_TRIG_PIN); _delay_us(20);
+        SET_BIT(GPIO_PORTB, USS_TRIG_PIN );     _delay_us(25);
+        CLEAR_BIT(GPIO_PORTB , USS_TRIG_PIN); _delay_us(20);
+        break;
+    case 'C' :
+        CLEAR_BIT(GPIO_PORTC , USS_TRIG_PIN); _delay_us(20);
+        SET_BIT(GPIO_PORTC, USS_TRIG_PIN );     _delay_us(25);
+        CLEAR_BIT(GPIO_PORTC , USS_TRIG_PIN); _delay_us(20);
+        break;
+    case 'D' :
+        CLEAR_BIT(GPIO_PORTD , USS_TRIG_PIN); _delay_us(20);
+        SET_BIT(GPIO_PORTD, USS_TRIG_PIN );     _delay_us(25);
+        CLEAR_BIT(GPIO_PORTD , USS_TRIG_PIN); _delay_us(20);
+        break;
+    }
 }
 
 u32 USS_u32GET_DISTANCE_mm(void)
 {
+    u8 Is_Echoed =FALSE;
+    
     USS_vPULL_THE_TRIGGER();
+    
     TMR2_vENABLE_CTC_INTERRUPT();
-    while( GET_BIT(ECHO_PIN, USS_ECHO_PIN) == LOW  );
-    TMR2_vSet_Prescaler(TMR_PRESCALE_8);
-    TMR2_vASSIGN_CTC_VALUE(USS_OCR_VALUE);
-    GlobalInterrupt_Enable();
-    while(time_ms == 0.0);
-    TMR2_vReset_Prescaler();
-    TMR2_vDISABLE_CTC_INTERRUPT();
-    time_ms=0.0;
+
+    f32 time_ms = (counter*USS_PERIOD_ms) + (TMR_TCNT2*USS_TIME_TICK_TIME_s*1000);
+
+
     return USS_MEASURE_DISTANCE_mm(time_ms);
 }
 
